304596ed59bd371801a20d26eb70dfba3c1f0778
Attect <attect@muka.app>
2026-04-24 19:44:32 +0800

    feat(camera): CameraPreview 全面迁移至 Camera2 API
    - 重写 CameraPreview 为 Camera2 实现，替代废弃的 android.hardware.Camera
    - 使用 CameraManager + CameraDevice + CameraCaptureSession 状态机架构
    - 后台 HandlerThread 管理相机生命周期，避免阻塞主线程
    - 保留 stop() 公共接口，BathActivity 调用方无需修改
    - 移除未使用的 setOneShotPreviewCallback/setPreviewCallback/getPreviewSize 等遗留接口

--- app/src/main/java/jp/co/a_tm/moeyu/CameraPreview.java
+++ app/src/main/java/jp/co/a_tm/moeyu/CameraPreview.java
@@ -1,278 +1,237 @@
 package jp.co.a_tm.moeyu;
 
 import android.app.Activity;
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
-import android.hardware.Camera.PreviewCallback;
-import android.hardware.Camera.Size;
-import android.os.Build.VERSION;
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
 import android.view.SurfaceHolder;
-import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
-import android.widget.FrameLayout.LayoutParams;
+import android.widget.FrameLayout;
 import android.widget.Toast;
-import java.io.IOException;
-import java.util.List;
+import java.util.Collections;
 
-public class CameraPreview extends SurfaceView implements Callback {
-    private static final String CAMERA_PARAM_LANDSCAPE = "landscape";
-    private static final String CAMERA_PARAM_ORIENTATION = "orientation";
-    private static final String CAMERA_PARAM_PORTRAIT = "portrait";
-    private static boolean DEBUGGING = false;
-    private static final String LOG_TAG = "CameraPreviewSample";
-    protected Activity mActivity;
-    protected Camera mCamera;
-    private int mCenterPosX = -1;
-    private int mCenterPosY;
-    private SurfaceHolder mHolder;
-    protected Size mPictureSize;
-    protected List<Size> mPictureSizeList;
-    PreviewReadyCallback mPreviewReadyCallback = null;
-    protected Size mPreviewSize;
-    protected List<Size> mPreviewSizeList;
-    private int mSurfaceChangedCallDepth = 0;
-    protected boolean mSurfaceConfiguring = false;
-
-    public interface PreviewReadyCallback {
-        void onPreviewReady();
-    }
+/**
+ * 相机预览视图（Camera2 API 实现）
+ * 作为 AR 模式背景，显示在最底层
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = "CameraPreview";
+    private final Activity mActivity;
+    private CameraDevice mCameraDevice;
+    private CameraCaptureSession mCaptureSession;
+    private CaptureRequest.Builder mPreviewRequestBuilder;
+    private HandlerThread mBackgroundThread;
+    private Handler mBackgroundHandler;
+    private Size mPreviewSize;
+    private boolean mSurfaceReady = false;
+    private boolean mCameraOpening = false;
 
     public CameraPreview(Activity activity) {
         super(activity);
         this.mActivity = activity;
-        this.mHolder = getHolder();
-        this.mHolder.addCallback(this);
-        this.mHolder.setType(3);
+        getHolder().addCallback(this);
     }
 
+    @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        if (VERSION.SDK_INT >= 9) {
-            this.mCamera = Camera.open(0);
-        } else {
-            this.mCamera = Camera.open();
-        }
-        Parameters cameraParams = this.mCamera.getParameters();
-        this.mPreviewSizeList = cameraParams.getSupportedPreviewSizes();
-        this.mPictureSizeList = cameraParams.getSupportedPictureSizes();
-        try {
-            this.mCamera.setPreviewDisplay(this.mHolder);
-        } catch (IOException e) {
-            this.mCamera.release();
-            this.mCamera = null;
-        }
+        mSurfaceReady = true;
+        startBackgroundThread();
+        openCamera();
     }
 
+    @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        this.mSurfaceChangedCallDepth++;
-        doSurfaceChanged(width, height);
-        this.mSurfaceChangedCallDepth--;
+        // Camera2 的会话会在配置时自动适配 Surface 尺寸
     }
 
-    private void doSurfaceChanged(int width, int height) {
-        this.mCamera.stopPreview();
-        Parameters cameraParams = this.mCamera.getParameters();
-        boolean portrait = isPortrait();
-        if (!this.mSurfaceConfiguring) {
-            Size previewSize = determinePreviewSize(portrait, width, height);
-            Size pictureSize = determinePictureSize(previewSize);
-            if (DEBUGGING) {
-                Log.v(LOG_TAG, "Desired Preview Size - w: " + width + ", h: " + height);
-            }
-            this.mPreviewSize = previewSize;
-            this.mPictureSize = pictureSize;
-            this.mSurfaceConfiguring = adjustSurfaceLayoutSize(previewSize, portrait, width, height);
-            if (this.mSurfaceConfiguring && this.mSurfaceChangedCallDepth <= 1) {
-                return;
-            }
-        }
-        configureCameraParameters(cameraParams, portrait);
-        this.mSurfaceConfiguring = false;
-        try {
-            this.mCamera.startPreview();
-        } catch (Exception e) {
-            Log.w(LOG_TAG, "Failed to start preview: " + e.getMessage());
-            this.mPreviewSizeList.remove(this.mPreviewSize);
-            this.mPreviewSize = null;
-            if (this.mPreviewSizeList.size() > 0) {
-                surfaceChanged(null, 0, width, height);
-            } else {
-                Toast.makeText(this.mActivity, "Can't start preview", Toast.LENGTH_LONG).show();
-                Log.w(LOG_TAG, "Gave up starting preview");
-            }
-        }
-        if (this.mPreviewReadyCallback != null) {
-            this.mPreviewReadyCallback.onPreviewReady();
-        }
-    }
-
-    public Size determinePreviewSize(boolean portrait, int reqWidth, int reqHeight) {
-        int reqPreviewWidth;
-        int reqPreviewHeight;
-        if (portrait) {
-            reqPreviewWidth = reqHeight;
-            reqPreviewHeight = reqWidth;
-        } else {
-            reqPreviewWidth = reqWidth;
-            reqPreviewHeight = reqHeight;
-        }
-        if (DEBUGGING) {
-            Log.v(LOG_TAG, "Listing all supported preview sizes");
-            for (Size size : this.mPreviewSizeList) {
-                Log.v(LOG_TAG, "  w: " + size.width + ", h: " + size.height);
-            }
-            Log.v(LOG_TAG, "Listing all supported picture sizes");
-            for (Size size2 : this.mPictureSizeList) {
-                Log.v(LOG_TAG, "  w: " + size2.width + ", h: " + size2.height);
-            }
-        }
-        float reqRatio = ((float) reqPreviewWidth) / ((float) reqPreviewHeight);
-        float deltaRatioMin = Float.MAX_VALUE;
-        Size retSize = null;
-        for (Size size22 : this.mPreviewSizeList) {
-            float deltaRatio = Math.abs(reqRatio - (((float) size22.width) / ((float) size22.height)));
-            if (deltaRatio < deltaRatioMin) {
-                deltaRatioMin = deltaRatio;
-                retSize = size22;
-            }
-        }
-        return retSize;
-    }
-
-    public Size determinePictureSize(Size previewSize) {
-        Size retSize = null;
-        for (Size size : this.mPictureSizeList) {
-            if (size.equals(previewSize)) {
-                return size;
-            }
-        }
-        if (DEBUGGING) {
-            Log.v(LOG_TAG, "Same picture size not found.");
-        }
-        float reqRatio = ((float) previewSize.width) / ((float) previewSize.height);
-        float deltaRatioMin = Float.MAX_VALUE;
-        for (Size size2 : this.mPictureSizeList) {
-            float deltaRatio = Math.abs(reqRatio - (((float) size2.width) / ((float) size2.height)));
-            if (deltaRatio < deltaRatioMin) {
-                deltaRatioMin = deltaRatio;
-                retSize = size2;
-            }
-        }
-        return retSize;
-    }
-
-    public boolean adjustSurfaceLayoutSize(Size previewSize, boolean portrait, int availableWidth, int availableHeight) {
-        float tmpLayoutHeight;
-        float tmpLayoutWidth;
-        float fact;
-        if (portrait) {
-            tmpLayoutHeight = (float) previewSize.width;
-            tmpLayoutWidth = (float) previewSize.height;
-        } else {
-            tmpLayoutHeight = (float) previewSize.height;
-            tmpLayoutWidth = (float) previewSize.width;
-        }
-        float factH = ((float) availableHeight) / tmpLayoutHeight;
-        float factW = ((float) availableWidth) / tmpLayoutWidth;
-        if (factH < factW) {
-            fact = factH;
-        } else {
-            fact = factW;
-        }
-        LayoutParams layoutParams = (LayoutParams) getLayoutParams();
-        int layoutHeight = (int) (tmpLayoutHeight * fact);
-        int layoutWidth = (int) (tmpLayoutWidth * fact);
-        if (DEBUGGING) {
-            Log.v(LOG_TAG, "Preview Layout Size - w: " + layoutWidth + ", h: " + layoutHeight);
-            Log.v(LOG_TAG, "Scale factor: " + fact);
-        }
-        if (layoutWidth == getWidth() && layoutHeight == getHeight()) {
-            return false;
-        }
-        layoutParams.height = layoutHeight;
-        layoutParams.width = layoutWidth;
-        if (this.mCenterPosX >= 0) {
-            layoutParams.topMargin = this.mCenterPosY - (layoutHeight / 2);
-            layoutParams.leftMargin = this.mCenterPosX - (layoutWidth / 2);
-        }
-        setLayoutParams(layoutParams);
-        return true;
-    }
-
-    public void setCenterPosition(int x, int y) {
-        this.mCenterPosX = x;
-        this.mCenterPosY = y;
-    }
-
-    public void configureCameraParameters(Parameters cameraParams, boolean portrait) {
-        if (VERSION.SDK_INT >= 8) {
-            int angle;
-            switch (this.mActivity.getWindowManager().getDefaultDisplay().getRotation()) {
-                case 0:
-                    angle = 90;
-                    break;
-                case 1:
-                    angle = 0;
-                    break;
-                case 2:
-                    angle = 270;
-                    break;
-                case 3:
-                    angle = 180;
-                    break;
-                default:
-                    angle = 90;
-                    break;
-            }
-            Log.v(LOG_TAG, "angle: " + angle);
-            this.mCamera.setDisplayOrientation(angle);
-        } else if (portrait) {
-            cameraParams.set(CAMERA_PARAM_ORIENTATION, CAMERA_PARAM_PORTRAIT);
-        } else {
-            cameraParams.set(CAMERA_PARAM_ORIENTATION, CAMERA_PARAM_LANDSCAPE);
-        }
-        cameraParams.setPreviewSize(this.mPreviewSize.width, this.mPreviewSize.height);
-        cameraParams.setPictureSize(this.mPictureSize.width, this.mPictureSize.height);
-        if (DEBUGGING) {
-            Log.v(LOG_TAG, "Preview Actual Size - w: " + this.mPreviewSize.width + ", h: " + this.mPreviewSize.height);
-            Log.v(LOG_TAG, "Picture Actual Size - w: " + this.mPictureSize.width + ", h: " + this.mPictureSize.height);
-        }
-        this.mCamera.setParameters(cameraParams);
-    }
-
+    @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
+        mSurfaceReady = false;
         stop();
     }
 
+    private void startBackgroundThread() {
+        mBackgroundThread = new HandlerThread("CameraBackground");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    private void stopBackgroundThread() {
+        if (mBackgroundThread != null) {
+            mBackgroundThread.quitSafely();
+            try {
+                mBackgroundThread.join();
+                mBackgroundThread = null;
+                mBackgroundHandler = null;
+            } catch (InterruptedException e) {
+                Log.e(TAG, "stopBackgroundThread interrupted", e);
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private void openCamera() {
+        if (mCameraOpening || !mSurfaceReady) return;
+        CameraManager manager = (CameraManager) mActivity.getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String[] cameraIds = manager.getCameraIdList();
+            if (cameraIds.length == 0) {
+                Toast.makeText(mActivity, "未检测到相机", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            String cameraId = cameraIds[0];
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            if (map == null) {
+                Toast.makeText(mActivity, "无法获取相机配置", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Size[] sizes = map.getOutputSizes(SurfaceHolder.class);
+            mPreviewSize = chooseOptimalSize(sizes, getWidth(), getHeight());
+            adjustSurfaceLayoutSize(mPreviewSize);
+            mCameraOpening = true;
+            manager.openCamera(cameraId, mStateCallback, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            Log.e(TAG, "openCamera failed", e);
+            Toast.makeText(mActivity, "相机访问失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 选择最优预览尺寸：优先 16:9，其次覆盖 surface 尺寸
+     */
+    private Size chooseOptimalSize(Size[] choices, int width, int height) {
+        if (choices == null || choices.length == 0) {
+            return new Size(640, 480);
+        }
+        float targetRatio = (float) Math.max(width, height) / Math.min(width, height);
+        Size optimalSize = choices[0];
+        float minDelta = Float.MAX_VALUE;
+        for (Size size : choices) {
+            float ratio = (float) Math.max(size.getWidth(), size.getHeight())
+                    / Math.min(size.getWidth(), size.getHeight());
+            float delta = Math.abs(ratio - targetRatio);
+            if (delta < minDelta) {
+                minDelta = delta;
+                optimalSize = size;
+            }
+        }
+        return optimalSize;
+    }
+
+    /**
+     * 根据预览尺寸调整 SurfaceView 布局，保持比例并居中填充
+     */
+    private void adjustSurfaceLayoutSize(Size previewSize) {
+        int availableWidth = getWidth();
+        int availableHeight = getHeight();
+        if (availableWidth == 0 || availableHeight == 0) return;
+
+        float previewRatio = (float) previewSize.getWidth() / previewSize.getHeight();
+        float screenRatio = (float) availableWidth / availableHeight;
+
+        int layoutWidth, layoutHeight;
+        if (screenRatio > previewRatio) {
+            layoutHeight = availableHeight;
+            layoutWidth = (int) (availableHeight * previewRatio);
+        } else {
+            layoutWidth = availableWidth;
+            layoutHeight = (int) (availableWidth / previewRatio);
+        }
+
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) getLayoutParams();
+        if (params == null) {
+            params = new FrameLayout.LayoutParams(layoutWidth, layoutHeight);
+        } else {
+            params.width = layoutWidth;
+            params.height = layoutHeight;
+        }
+        setLayoutParams(params);
+    }
+
+    private final CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice cameraDevice) {
+            mCameraOpening = false;
+            mCameraDevice = cameraDevice;
+            createCameraPreviewSession();
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice cameraDevice) {
+            mCameraOpening = false;
+            cameraDevice.close();
+            mCameraDevice = null;
+        }
+
+        @Override
+        public void onError(CameraDevice cameraDevice, int error) {
+            mCameraOpening = false;
+            cameraDevice.close();
+            mCameraDevice = null;
+            Log.e(TAG, "Camera error: " + error);
+            Toast.makeText(mActivity, "相机错误: " + error, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private void createCameraPreviewSession() {
+        try {
+            Surface surface = getHolder().getSurface();
+            mPreviewRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mPreviewRequestBuilder.addTarget(surface);
+
+            mCameraDevice.createCaptureSession(
+                    Collections.singletonList(surface),
+                    new CameraCaptureSession.StateCallback() {
+                        @Override
+                        public void onConfigured(CameraCaptureSession session) {
+                            if (mCameraDevice == null) return;
+                            mCaptureSession = session;
+                            try {
+                                mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
+                                        CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                                mCaptureSession.setRepeatingRequest(
+                                        mPreviewRequestBuilder.build(), null, mBackgroundHandler);
+                            } catch (CameraAccessException e) {
+                                Log.e(TAG, "setRepeatingRequest failed", e);
+                            }
+                        }
+
+                        @Override
+                        public void onConfigureFailed(CameraCaptureSession session) {
+                            Toast.makeText(mActivity, "相机配置失败", Toast.LENGTH_SHORT).show();
+                        }
+                    },
+                    mBackgroundHandler
+            );
+        } catch (CameraAccessException e) {
+            Log.e(TAG, "createCameraPreviewSession failed", e);
+        }
+    }
+
+    /**
+     * 停止预览并释放相机资源
+     */
     public void stop() {
-        if (this.mCamera != null) {
-            this.mCamera.stopPreview();
-            this.mCamera.release();
-            this.mCamera = null;
+        if (mCaptureSession != null) {
+            mCaptureSession.close();
+            mCaptureSession = null;
         }
-    }
-
-    public boolean isPortrait() {
-        return this.mActivity.getResources().getConfiguration().orientation == 1;
-    }
-
-    public void setOneShotPreviewCallback(PreviewCallback callback) {
-        if (this.mCamera != null) {
-            this.mCamera.setOneShotPreviewCallback(callback);
+        if (mCameraDevice != null) {
+            mCameraDevice.close();
+            mCameraDevice = null;
         }
-    }
-
-    public void setPreviewCallback(PreviewCallback callback) {
-        if (this.mCamera != null) {
-            this.mCamera.setPreviewCallback(callback);
-        }
-    }
-
-    public Size getPreviewSize() {
-        return this.mPreviewSize;
-    }
-
-    public void setOnPreviewReady(PreviewReadyCallback cb) {
-        this.mPreviewReadyCallback = cb;
+        stopBackgroundThread();
+        mCameraOpening = false;
     }
 }
